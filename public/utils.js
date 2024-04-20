78a70d1bd03503965d694eab7a336c1b23feb19e
GJSegatto <gabrieljulianigj@gmail.com>
2024-04-20 16:45:37 -0300

    Trabalho finalizado

--- public/utils.js
+++ public/utils.js
@@ -1,7 +1,14 @@
 const btnClick = document.querySelectorAll('.btn');
+
 btnClick.forEach(btnClicked => {
     btnClicked.addEventListener('click', () => {
         btnClick.forEach(btn => btn.classList.remove('special')); 
         btnClicked.classList.add('special');
     });
 });
+
+function showApp() {
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('app').style.display = 'flex'; 
+}
+
